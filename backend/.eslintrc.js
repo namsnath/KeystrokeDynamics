68eb6a8d0e613f527cc29a17c3d48e38631ccb5f
Namit Nathwani <namsnath@gmail.com>
2020-07-31 20:01:02 +0530

    feat: sets up basic utils for backend

---
+++ backend/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-param-reassign': 0,
+    'no-underscore-dangle': 0,
+    'max-len': 1,
+    'no-async-promise-executor': 1,
+  },
+};
